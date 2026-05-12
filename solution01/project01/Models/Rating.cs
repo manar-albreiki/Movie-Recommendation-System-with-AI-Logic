6b522b4cee5091c0afb3f409acc0af3a6a9dbb9c
“manar <albreikimanar@gmail.com>
2026-05-12 14:11:14 +0400

    create folder models and 4 classes

---
+++ solution01/project01/Models/Rating.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project01.Models
+{
+    internal class Rating
+    {
+    }
+}
